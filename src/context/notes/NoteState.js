25cb5851e131703d0c3da7864b46b51b25d9468e
shirazsiddiqui <shirazsiddiqui016@gmail.com>
2021-11-24 11:34:19 +0530

    delete note completed

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -69,8 +69,11 @@ const NoteState = (props)=>{
           setNotes (notes.concat(note))
         }
       // Delete a Note
-        const deleteNote = () => {
-
+        const deleteNote = (id) => {
+          //TODO : API call
+          console.log("Deleting the note with id" + id);
+          const newNotes = notes.filter((note)=>{return note._id!==id})
+          setNotes(newNotes)
         }
       // Edit a Note
         const editNote = () => {
